8113157a31753d76e1df8414f5499a013bb77556
Dmytro <kobylynskiy.d@outlook.com>
2016-11-16 20:35:17 +0200

    Add views MyOffers and MyOrders

---
+++ src/Identity/Controllers/OrderController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Identity.Data;
+using Identity.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Identity.Controllers
+{
+    public class OrderController : Controller
+    {
+        UserManager<ApplicationUser> _userManager;
+        //private ApplicationDbContext db;
+        public async Task TaxiOrders()
+        {
+            
+        }
+        public IActionResult CreateTaxiOrder()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateTaxiOrder(TaxiOrder taxiOrder)
+        {
+            Console.WriteLine(_userManager.Users.ToList());
+            //var user = await GetCurrentUserAsync();
+            //var userId = user?.Id;
+            //taxiOrder.OrderOwnerId=userId;
+            //db.TaxiOrders.Add(taxiOrder);
+            // await db.SaveChangesAsync();
+            return RedirectToAction("TaxiOrders");
+        }
+
+    }
+}
---
+++ src/Identity/Data/Migrations/20161116135748_OwnerIDMigr.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Identity.Data.Migrations
+{
+    public partial class OwnerIDMigr : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "OrderOwnerId",
+                table: "TaxiOrders",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "OfferOwnerId",
+                table: "TaxiOffers",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "OrderOwnerId",
+                table: "TaxiOrders");
+
+            migrationBuilder.DropColumn(
+                name: "OfferOwnerId",
+                table: "TaxiOffers");
+        }
+    }
+}
